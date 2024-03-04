744da2510a38fd2f6d87977f44833f87d1c87660
Dmitriy Korobov <39617069+Scalagrammer@users.noreply.github.com>
2024-03-04 21:28:36 +0300

    Update TailRec.kt

--- TailRec.kt
+++ TailRec.kt
@@ -1,25 +1,35 @@
-import java.util.*
 import kotlin.reflect.KProperty
+import java.util.LinkedList
 
-class TailRecScope<R>(private val f : TailRecScope<R>.(R) -> TailRec<R>) {
+typealias Endo<A> = (A) -> A
 
-    fun tailcall(value : R) : TailRec<R> = Suspend { f(value) }
-
-    infix fun R.pureIf(condition : Boolean) : TailRec<R>? = takeIf { condition }?.let(::Pure)
-    
-    infix operator fun TailRec<Long>.plus(operand : TailRec<Long>) : TailRec<Long> = FlatMap(this) { a -> Map(operand) { b -> a + b } }
-    
+class TailRecScope<A, R>(private val f : TailRecScope<A, R>.(A) -> TailRec<R>) {
+    fun tailcall(value : A) : TailRec<R> = Suspend { f(value) }
 }
 
-sealed interface TailRec<R> {
-    companion object {
-        operator fun <R> invoke(zero : R, scope : TailRecScope<R>.(R) -> TailRec<R>) : TailRec<R> = TailRecScope(scope).scope(zero)
+operator fun <R> TailRec<R>.getValue(r : Nothing?, p : KProperty<*>?) : R = eval()
+
+infix fun <R> R.doneIf(condition : Boolean) : TailRec<R>? = takeIf { condition }?.let { Pure(it) }
+
+infix operator fun TailRec<Long>.plus(operand : TailRec<Long>) : TailRec<Long> = FlatMap(this) { a -> Map(operand) { b -> a + b } }
+
+fun <A, R> tailrec(scope : TailRecScope<A, R>.(A) -> TailRec<R>) : TailRec<(A) -> R> = TailRecScope(scope).run { Pure { scope(it).eval() } }
+
+fun main() {
+
+    val fibonacci : Endo<Long> by tailrec {
+        it doneIf(it <= 1) ?: tailcall(it - 1) + tailcall(it - 2)
     }
+
+    println(fibonacci(35))
+
 }
 
-data class Map<T, R>(val source : TailRec<T>, val transform : (T) -> R) : TailRec<R>
+sealed interface TailRec<R>
 
 data class FlatMap<T, R>(val source : TailRec<T>, val transform : (T) -> TailRec<R>) : TailRec<R>
+
+data class Map<T, R>(val source : TailRec<T>, val transform : (T) -> R) : TailRec<R>
 
 data class Suspend<R>(val resume : () -> TailRec<R>) : TailRec<R>
 
@@ -27,40 +37,30 @@ data class Delay<R>(val value : () -> R) : TailRec<R>
 
 data class Pure<R>(val value : R) : TailRec<R>
 
-fun main() {
-
-    val fibonacci by TailRec(zero = 20L) {
-        it pureIf(it <= 1L) ?: tailcall(it - 1) + tailcall(it - 2)
-    }
-
-    println(fibonacci)
-
-}
-
 @Suppress("UNCHECKED_CAST")
-operator fun <R> TailRec<R>.getValue(r : Nothing?, p : KProperty<*>?) : R {
+fun <R> TailRec<R>.eval() : R {
 
-    val bindRest = Stack<((Any) -> TailRec<Any>)>()
+    val bindRest = LinkedList<(Any) -> TailRec<Any>>()
 
-    var bindFirst: ((Any) -> TailRec<Any>)? = null
+    var bindFirst : ((Any) -> TailRec<Any>)? = null
 
-    var source: TailRec<*>? = this
+    var source : TailRec<*>? = this
 
-    var result: Any? = null
+    var result : Any? = null
 
     while (true) {
         if (source != null) when (source) {
             is FlatMap<*, *> -> {
                 bindFirst?.let(bindRest::push)
                 bindFirst = source.transform as (Any) -> TailRec<Any>
-                source = source.source
+                source    = source.source
             }
 
             is Map<*, *> -> {
                 bindFirst?.let(bindRest::push)
                 val transform = source.transform as (Any) -> Any
-                bindFirst = { Delay { transform(it) } }
-                source = source.source
+                bindFirst     = { Delay { transform(it) } }
+                source        = source.source
             }
 
             is Pure<*> -> {
@@ -73,27 +73,29 @@ operator fun <R> TailRec<R>.getValue(r : Nothing?, p : KProperty<*>?) : R {
                 source = null
             }
 
-            is Suspend<*> ->
+            is Suspend<*> -> {
                 source = source.resume()
+            }
         }
 
         when {
             result != null && bindFirst != null -> {
-                source = bindFirst(result)
-                result = null
+                source    = bindFirst(result)
+                result    = null
                 bindFirst = null
                 continue
             }
 
             result != null && bindRest.isNotEmpty() -> {
-                source = (bindRest.pop())(result)
-                result = null
+                source    = (bindRest.pop())(result)
+                result    = null
                 bindFirst = null
                 continue
             }
 
-            result != null ->
+            result != null -> {
                 return result as R
+            }
         }
     }
 }
