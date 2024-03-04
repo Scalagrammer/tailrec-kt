0cd47956080f70a719fdc8ab570c3e784b77f46e
Dmitriy Korobov <39617069+Scalagrammer@users.noreply.github.com>
2024-03-04 20:05:24 +0300

    Update TailRec.kt

--- TailRec.kt
+++ TailRec.kt
@@ -2,13 +2,13 @@ import java.util.*
 import kotlin.reflect.KProperty
 
 class TailRecScope<R>(private val f : TailRecScope<R>.(R) -> TailRec<R>) {
-    
+
     fun tailcall(value : R) : TailRec<R> = Suspend { f(value) }
 
-    fun done(value : R) : TailRec<R> = Done(value)
-
-    infix operator fun TailRec<Long>.plus(operand : TailRec<Long>) : TailRec<Long> =
-        FlatMap(this) { a -> Map(operand) { b -> a + b } }
+    infix fun R.pureIf(condition : Boolean) : TailRec<R>? = takeIf { condition }?.let(::Pure)
+    
+    infix operator fun TailRec<Long>.plus(operand : TailRec<Long>) : TailRec<Long> = FlatMap(this) { a -> Map(operand) { b -> a + b } }
+    
 }
 
 sealed interface TailRec<R> {
@@ -23,12 +23,14 @@ data class FlatMap<T, R>(val source : TailRec<T>, val transform : (T) -> TailRec
 
 data class Suspend<R>(val resume : () -> TailRec<R>) : TailRec<R>
 
-data class Done<R>(val result : R) : TailRec<R>
+data class Delay<R>(val value : () -> R) : TailRec<R>
+
+data class Pure<R>(val value : R) : TailRec<R>
 
 fun main() {
 
-    val fibonacci by TailRec(zero = 35L) { 
-        if (it <= 1L) done(it) else tailcall(it - 1) + tailcall(it - 2)
+    val fibonacci by TailRec(zero = 20L) {
+        it pureIf(it <= 1L) ?: tailcall(it - 1) + tailcall(it - 2)
     }
 
     println(fibonacci)
@@ -47,24 +49,27 @@ operator fun <R> TailRec<R>.getValue(r : Nothing?, p : KProperty<*>?) : R {
     var result: Any? = null
 
     while (true) {
-
         if (source != null) when (source) {
-
             is FlatMap<*, *> -> {
                 bindFirst?.let(bindRest::push)
-                bindFirst = source.transform as ((Any) -> TailRec<Any>)
+                bindFirst = source.transform as (Any) -> TailRec<Any>
                 source = source.source
             }
 
             is Map<*, *> -> {
                 bindFirst?.let(bindRest::push)
-                val mapper = source.transform as ((Any) -> Any)
-                bindFirst = { Done(mapper(it)) }
+                val transform = source.transform as (Any) -> Any
+                bindFirst = { Delay { transform(it) } }
                 source = source.source
             }
 
-            is Done<*> -> {
-                result = source.result
+            is Pure<*> -> {
+                result = source.value
+                source = null
+            }
+
+            is Delay<*> -> {
+                result = source.value()
                 source = null
             }
 
